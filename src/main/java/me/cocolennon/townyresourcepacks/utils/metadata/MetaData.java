17c1b9ae798533f53969f02c208464bb2c16ee72
Cocolennon <cocolennong@gmail.com>
2023-12-16 15:17:04 +0100

    Added instance to MetaData to avoid static statements

--- src/main/java/me/cocolennon/townyresourcepacks/utils/metadata/MetaData.java
+++ src/main/java/me/cocolennon/townyresourcepacks/utils/metadata/MetaData.java
@@ -5,17 +5,22 @@ import com.palmergames.bukkit.towny.object.metadata.StringDataField;
 import com.palmergames.bukkit.towny.utils.MetaDataUtil;
 
 public class MetaData {
-    public static StringDataField resourcePackLink = new StringDataField("townyresourcepack_link");
+    private static MetaData instance = new MetaData();
+    public StringDataField resourcePackLink = new StringDataField("townyresourcepack_link");
 
-    public static String getResourcePackLink(Town town){
+    public  String getResourcePackLink(Town town){
         return MetaDataUtil.getString(town, resourcePackLink);
     }
 
-    public static void setResourcePackLink(Town town, String resourceLink){
+    public void setResourcePackLink(Town town, String resourceLink){
         if(!town.hasMeta("townyresourcepack_link")){
             MetaDataUtil.addNewStringMeta(town, "townyresourcepack_link", resourceLink, true);
             return;
         }
         MetaDataUtil.setString(town, resourcePackLink, resourceLink, true);
+    }
+
+    public static MetaData getInstance() {
+        return instance;
     }
 }
