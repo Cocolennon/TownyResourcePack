2b91efcfd30d3af17c7b4cd7f58308a01f5b4d71
Cocolennon <cocolennong@gmail.com>
2023-12-16 15:16:18 +0100

    Added Update Checker

--- src/main/java/me/cocolennon/townyresourcepacks/Main.java
+++ src/main/java/me/cocolennon/townyresourcepacks/Main.java
@@ -5,18 +5,45 @@ import com.palmergames.bukkit.towny.TownyCommandAddonAPI.CommandType;
 import me.cocolennon.townyresourcepacks.commands.CommandResourcePack;
 import me.cocolennon.townyresourcepacks.listeners.PlayerEnterTown;
 import me.cocolennon.townyresourcepacks.listeners.PlayerExitTown;
+import me.cocolennon.townyresourcepacks.utils.UpdateChecker;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Arrays;
 
 public class Main extends JavaPlugin {
     public static Main instance;
+    FileConfiguration config = getConfig();
+    private String version;
+    private String latestVersion;
+    private boolean usingOldVersion = false;
 
     @Override
     public void onEnable() {
-        getLogger().info("Plugin enabled!");
+        instance = this;
+        checkVersion();
+        setupConfig();
         registerCommands();
         registerListeners();
+        getLogger().info("Plugin enabled!");
+    }
 
-        instance = this;
+    public void checkVersion() {
+        new UpdateChecker(this, 105579).getVersion(cVersion -> {
+            version = this.getDescription().getVersion();
+            latestVersion = cVersion;
+            if (!getVersion().equals(cVersion)) {
+                getLogger().info("You are using an older version of Top Luck, please update to version " + cVersion);
+                usingOldVersion = true;
+            }
+        });
+    }
+
+    private void setupConfig() {
+        config.addDefault("default-resource-pack", "https://www.curseforge.com/api/v1/mods/457153/files/4572162/download");
+        config.options().setHeader(Arrays.asList("default-resource-pack: The resource pack that will be used when a player is outside of a town"));
+        config.options().copyDefaults(true);
+        saveConfig();
     }
 
     private void registerCommands(){
@@ -27,5 +54,9 @@ public class Main extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new PlayerExitTown(), this);
         getServer().getPluginManager().registerEvents(new PlayerEnterTown(), this);
     }
+
+    public String getVersion() { return version; }
+    public String getLatestVersion(){ return latestVersion; }
+    public boolean getUsingOldVersion() { return usingOldVersion; }
     public static Main getInstance() { return instance; }
 }
