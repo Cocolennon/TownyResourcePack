978680f30b6a85a80d10ca744db8018475b7d3a5
Cocolennon <cocolennong@gmail.com>
2023-12-16 15:20:02 +0100

    Made instance final

--- src/main/java/me/cocolennon/townyresourcepacks/utils/metadata/MetaData.java
+++ src/main/java/me/cocolennon/townyresourcepacks/utils/metadata/MetaData.java
@@ -5,7 +5,7 @@ import com.palmergames.bukkit.towny.object.metadata.StringDataField;
 import com.palmergames.bukkit.towny.utils.MetaDataUtil;
 
 public class MetaData {
-    private static MetaData instance = new MetaData();
+    private static final MetaData instance = new MetaData();
     public StringDataField resourcePackLink = new StringDataField("townyresourcepack_link");
 
     public  String getResourcePackLink(Town town){
